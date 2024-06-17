bea29c7556ac5eff3db8415ca1cdf4b001874ec9
David Nwachukwu <davidnw1998@gmail.com>
2024-06-17 08:16:46 +0100

    better app design

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -19,29 +19,55 @@ struct ContentView: View {
 	
     var body: some View {
 		ZStack {
-			LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+			RadialGradient(stops: [
+				.init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+				.init(color: Color(red: 0.76, green: 0.15, blue: 0.26 ), location: 0.3)
+			], center: .top, startRadius: 200, endRadius: 700)
 				.ignoresSafeArea()
 			
-			VStack(spacing: 30) {
-				VStack {
-					Text("Tap the flag of")
-						.font(.subheadline.weight(.heavy))
-					
-					Text(countries[correctAnswer])
-						.font(.largeTitle.weight(.semibold))
-				}
-				.foregroundStyle(.white)
+			VStack {
+				Spacer()
 				
-				ForEach(0..<3) { number in
-					Button {
-						flagTapped(number)
-					} label: {
-						Image(countries[number])
-							.clipShape(Capsule())
-							.shadow(radius: 5)
+				Text("Guess the Flag")
+					.font(.largeTitle.bold())
+					.foregroundStyle(.white)
+				
+				VStack(spacing: 15) {
+					VStack {
+						Text("Tap the flag of")
+							.font(.subheadline.weight(.heavy))
+							.foregroundStyle(.secondary)
+						
+						Text(countries[correctAnswer])
+							.font(.largeTitle.weight(.semibold))
+					}
+					
+					ForEach(0..<3) { number in
+						Button {
+							flagTapped(number)
+						} label: {
+							Image(countries[number])
+								.clipShape(Capsule())
+								.shadow(radius: 5)
+						}
 					}
 				}
+				.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+				.padding(.vertical, 20)
+				.background(.regularMaterial)
+				.clipShape(RoundedRectangle(cornerRadius: 20))
+				
+				Spacer()
+				Spacer()
+				
+				Text("Score: \(totalScore)/\(questionsAnswered)")
+					.foregroundStyle(.white)
+					.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/.bold())
+				
+				Spacer()
 			}
+			.padding()
+			
 		}
 		.alert(scoreTitle, isPresented: $showingScore) {
 			Button("Continue", action: askQuestion)
